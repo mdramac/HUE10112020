9bf09c993ca2e07527c666466a6a48c87f4411cf
lzieberm <lzieberm@htl-steyr.ac.at>
2020-11-23 14:35:15 +0100

    Änderungen beim Laden

--- src/application/controller/Controller.java
+++ src/application/controller/Controller.java
@@ -1,8 +1,18 @@
 package application.controller;
 
+import application.MyFXMLLoader;
 import javafx.event.ActionEvent;
 
 public class Controller {
+    public void editStaticlicked(ActionEvent actionEvent) {
+        MyFXMLLoader loader = new MyFXMLLoader();
+        loader.loadFXML("view/stati.fxml", "Stati bearbeiten");
+    }
+
+    public void editPrioritiesclicked(ActionEvent actionEvent) {
+        MyFXMLLoader loader = new MyFXMLLoader();
+        loader.loadFXML("view/priorities.fxml" , "Prioritäten bearbeiten");
+    }
 
 //gs
 }
