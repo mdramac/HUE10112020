7c84fda2f0e82e0b7776b66c28822a07d934a04a
lzieberm <lzieberm@htl-steyr.ac.at>
2020-11-25 10:29:47 +0100

    Änderungen beim Laden

---
+++ src/application/controller/PriorityController.java
@@ -0,0 +1,19 @@
+package application.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class PriorityController {
+    public ListView priorityListview;
+    public TextField priorityTextfield;
+
+    public void newClicked(ActionEvent actionEvent) {
+    }
+
+    public void saveClicked(ActionEvent actionEvent) {
+    }
+
+    public void deleteClicked(ActionEvent actionEvent) {
+    }
+}
